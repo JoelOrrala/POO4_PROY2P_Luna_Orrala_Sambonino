290a1acec9c3878f16500002a1dc779c7eeef2a4
Luis A. Luna A <laluna@espol.edu.ec>
2023-01-09 17:01:02 -0500

    Vista creada asegurada

--- POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/App.java
+++ POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/App.java
@@ -12,27 +12,28 @@ import java.io.IOException;
  * JavaFX App
  */
 public class App extends Application {
-
+    
     private static Scene scene;
-
+    
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("VentanaIngreso"), 640, 480);
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("The Good Burguer Restaurant");
     }
-
+    
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
-
+    
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
-
+    
     public static void main(String[] args) {
         launch();
     }
-
+    
 }
--- POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/module-info.java
+++ POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.pooespol.poo4_proy2p_luna_orrala_sambonino {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.pooespol.poo4_proy2p_luna_orrala_sambonino to javafx.fxml;
     exports com.pooespol.poo4_proy2p_luna_orrala_sambonino;
