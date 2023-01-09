7c2914e599b268aa5a397f0661815125acb94b27
Luis A. Luna A <laluna@espol.edu.ec>
2023-01-09 15:45:54 -0500

    Proyect FXML creado

---
+++ POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.pooespol.poo4_proy2p_luna_orrala_sambonino {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.pooespol.poo4_proy2p_luna_orrala_sambonino to javafx.fxml;
+    exports com.pooespol.poo4_proy2p_luna_orrala_sambonino;
+}
