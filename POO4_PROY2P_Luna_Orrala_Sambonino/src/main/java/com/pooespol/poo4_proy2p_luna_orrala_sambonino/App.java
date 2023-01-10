b69630c51881ab5a27b8c1099abdbc389f067af4
JoelOrrala <jjorrala@espol.edu.ec>
2023-01-10 01:54:54 -0500

    Actualizacion

--- POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/App.java
+++ POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/App.java
@@ -12,28 +12,25 @@ import java.io.IOException;
  * JavaFX App
  */
 public class App extends Application {
-    
+
     private static Scene scene;
-    
+
+    public static String pathFiles = "src/main/resources/files/";
+    public static String pathImg = "src/main/resources/images/";
+
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("VentanaIngreso"), 640, 480);
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmLoader = new FXMLLoader(App.class.getResource("VentanaIngreso.fxml"));
+
+        Parent root = fxmLoader.load();
+        scene = new Scene(root, 640, 600);
         stage.setScene(scene);
         stage.show();
-        stage.setTitle("The Good Burguer Restaurant");
+
     }
-    
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-    
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-    
+
     public static void main(String[] args) {
         launch();
     }
-    
+
 }
---
+++ POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_modelo/Local.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pooespol.poo4_proy2p_modelo;
+
+/**
+ *
+ * @author joelorrala
+ */
+public class Local {
+    
+}
