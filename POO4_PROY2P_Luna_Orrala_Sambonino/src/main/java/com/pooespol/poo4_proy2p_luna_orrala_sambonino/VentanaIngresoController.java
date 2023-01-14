1cc93d1621e1b50fabbe9d5a3f35d7a04fc83ef9
Luis Alberto Luna Arana <laluna@espol.edu.ec>
2023-01-14 14:08:39 -0500

    Nueva ventana creada VentanaUsuario

--- POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/VentanaIngresoController.java
+++ POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/VentanaIngresoController.java
@@ -4,11 +4,18 @@
  */
 package com.pooespol.poo4_proy2p_luna_orrala_sambonino;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -24,10 +31,26 @@ public class VentanaIngresoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-    
     @FXML
-    public void ingresar(ActionEvent e){
-        
+    Pane root;
+
+    @FXML
+    public void ingresar(ActionEvent e) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/src/main/resources/com.pooespol.poo4_proy2p_luna_orrala_sambonino/VentanaUsuario.fxml"));
+        try {
+
+            root = loader.load();
+            VentanaUsuarioController controlador = loader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.showAndWait();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
     }
-    
+
 }
