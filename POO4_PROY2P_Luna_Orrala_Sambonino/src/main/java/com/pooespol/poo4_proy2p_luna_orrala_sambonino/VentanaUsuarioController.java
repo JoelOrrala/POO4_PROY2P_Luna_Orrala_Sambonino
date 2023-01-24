182e4eafb2dd3debbe7c6f7f4e81f40054c854ee
Luis Alberto Luna Arana <laluna@espol.edu.ec>
2023-01-24 17:03:39 -0500

    Creacion de la ventana LocalCercano con su mapa

--- POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/VentanaUsuarioController.java
+++ POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/VentanaUsuarioController.java
@@ -4,11 +4,19 @@
  */
 package com.pooespol.poo4_proy2p_luna_orrala_sambonino;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -27,6 +35,20 @@ public class VentanaUsuarioController implements Initializable {
 
     @FXML
     public void encontrarLocalCercano(ActionEvent e) {
+        ImageView imgV = null;
+        try ( FileInputStream input = new FileInputStream(App.pathImg + "mapa2.png")) {
+            Image imagen = new Image(input, 1100, 600, false, false);
+            imgV = new ImageView(imagen);
+
+        } catch (IOException ex) {
+            System.out.println("No se encontro la imagen");
+        }
+        Pane rootLocalCercano = new Pane();
+        rootLocalCercano.getChildren().addAll(imgV);
+        Scene scene = new Scene(rootLocalCercano, 1100, 600);
+        Stage s = new Stage();
+        s.setScene(scene);
+        s.show();
 
     }
 
