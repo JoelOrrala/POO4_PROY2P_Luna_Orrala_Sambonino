c0231efa7f58d1c6cda75f9680ad011755fa2005
Giovanni Sambonino <87047313+YuyoAndre@users.noreply.github.com>
2023-01-27 19:01:24 -0500

    VentanaDespedida Terminada

--- POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/App.java
+++ POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/App.java
@@ -22,7 +22,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmLoader = new FXMLLoader(App.class.getResource("VentanaIngreso.fxml"));
+        FXMLLoader fxmLoader = new FXMLLoader(App.class.getResource("VentanaDespedida.fxml"));
         Parent root = fxmLoader.load();
         scene = new Scene(root, 690,470);
         scene.getStylesheets().add(App.class.getResource("ingreso.css").toExternalForm());
--- POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/VentanaDespedidaController.java
+++ POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/VentanaDespedidaController.java
@@ -6,7 +6,20 @@ package com.pooespol.poo4_proy2p_luna_orrala_sambonino;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import com.pooespol.poo4_proy2p_modelo.Pago;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -18,9 +31,65 @@ public class VentanaDespedidaController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    @FXML 
+    Label lblMensajeDespedida;
+    
+    @FXML 
+    ImageView imgdespedida;
+    
+    @FXML
+    Label lblCerrarventana;
+    
+    @FXML
+    VBox _rootdespedida;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        
+        lblMensajeDespedida.setText("Su pedido N° ha sido pagado con exito y ahora empezaremos a prepararlo.\n" 
+                    +"En aproximadamente 30 minutos llegara a su destino.\n"
+                    +"Gracias por preferirnos.");
+        try ( FileInputStream input = new FileInputStream(new File(App.pathImg+"delivery_final.png"))) {
+            Image img2 = new Image(input);
+            imgdespedida.setImage(img2);
+        } catch (FileNotFoundException fnf) {
+            System.out.println("No se encontro el archivo");
+        } catch (IOException ioe) {
+            System.out.println("Io Exception");
+        }
+        crearThreadNuevaVentana(lblCerrarventana);
+        
+        
+        
+    }
+    public void crearThreadNuevaVentana(Label labelCont) {
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 5; i > -1; i--) {
+                    String mensaje = "Cerrando en "+ i + ".....";
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            labelCont.setText(mensaje);
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                        
+                    } catch (InterruptedException ex) {
+                    }
+                }
+                Stage stage = (Stage)_rootdespedida.getScene().getWindow();
+                Platform.runLater(() -> stage.close());
+            }
+        });
+        t2.start();
+        
     }
 
 }
