05eee8a7a8cc52328f829390646cb1c2650b78bc
Giovanni Sambonino <87047313+YuyoAndre@users.noreply.github.com>
2023-01-29 17:56:10 -0500

    correcciones finales

---
+++ POO4_PROY2P_Luna_Orrala_Sambonino/src/main/java/com/pooespol/poo4_proy2p_luna_orrala_sambonino/Principal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pooespol.poo4_proy2p_luna_orrala_sambonino;
+
+/**
+ *
+ * @author Giovanni
+ */
+public class Principal {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
